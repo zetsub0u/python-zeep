6580710ae0ffabc10f088cdc791cbe8af6ae0fa9
Alejandro <tovarich@gmail.com>
2016-05-10 23:31:02 +0200

    receive any requests parameter, like auth
    this is not ideal as some options might break things, but at least its your responsibility to pass undocumented options.
    this allows you to pass arguments like auth for basic auth

--- src/zeep/transports.py
+++ src/zeep/transports.py
@@ -5,10 +5,11 @@ from zeep.cache import SqliteCache
 
 class Transport(object):
 
-    def __init__(self, cache=None, timeout=300, verify=True):
+    def __init__(self, cache=None, timeout=300, verify=True, **kwargs):
         self.cache = cache or SqliteCache()
         self.timeout = timeout
         self.verify = verify
+        self.request_args = kwargs
 
     def load(self, url):
         if self.cache:
@@ -16,7 +17,7 @@ class Transport(object):
             if response:
                 return bytes(response)
 
-        response = requests.get(url, timeout=self.timeout, verify=self.verify)
+        response = requests.get(url, timeout=self.timeout, verify=self.verify, **self.request_args)
 
         if self.cache:
             self.cache.add(url, response.content)
@@ -25,6 +26,6 @@ class Transport(object):
 
     def post(self, address, message, headers):
         response = requests.post(
-            address, data=message, headers=headers, verify=self.verify
+            address, data=message, headers=headers, verify=self.verify, **self.request_args
         )
         return response
